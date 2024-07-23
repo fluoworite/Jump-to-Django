f0c223885aca81a31a01d47f4b664863c4f31cc8
Hyeongseok <a1932a@naver.com>
2024-07-23 13:52:54 +0900

    3.01~3.06

--- pybo/views.py
+++ pybo/views.py
@@ -1,8 +1,12 @@
+from urllib import request
+
+import requests
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseNotAllowed
 from django.urls import reverse_lazy
 from django.utils import timezone
 from django.views.generic import ListView, DetailView, CreateView
+from django.core.paginator import Paginator
 
 from . import form
 from .form import AnswerForm, QuestionForm
@@ -13,6 +17,11 @@ class QuestionListView(ListView):
     template_name = 'pybo/question_list.html'
     context_object_name = 'question_list'
     ordering = ['-create_date']
+    paginate_by = 10
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_obj'] = context.get('page_obj')
+        return context
 
 class QuestionDetailView(DetailView):
     model = Question
@@ -30,7 +39,11 @@ class AnswerCreateView(CreateView):
         answer.question = question
         answer.create_date = timezone.now()
         answer.save()
-        return redirect('pybo:detail', question_id=question.pk)
+        return redirect('pybo:detail', pk=question.id)
+
+    def form_invalid(self, form):
+        context = {'question': get_object_or_404(Question, pk=self.kwargs.get('question_id')), 'form': form}
+        return self.render_to_response(context)
 
     def get(self, request, *args, **kwargs):
         return HttpResponseNotAllowed('Only POST is possible.')
@@ -46,3 +59,6 @@ class QuestionCreateView(CreateView):
         question.create_date = timezone.now()
         question.save()
         return super().form_valid(form)
+
+def index(request):
+    return QuestionListView.as_view()(request)
